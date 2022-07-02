8689b0a762609b174de0a998d387717c258302b8
dekmas08 <86956343+dekmas08@users.noreply.github.com>
2022-07-02 14:58:39 +0800

    Update Association Rule CV. Jaya Utama Teknik-TA (1).py

--- Association Rule CV. Jaya Utama Teknik-TA (1).py
+++ Association Rule CV. Jaya Utama Teknik-TA (1).py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import pandas as pd
 import mlxtend as mlxtend
 import matplotlib.pyplot as plt 
@@ -11,23 +5,11 @@ import seaborn as sns
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
-
-# In[ ]:
-
-
 df = pd.read_excel('olah.xlsx')
 df
 
-
-# In[ ]:
-
-
 barang = df['Item'].value_counts()
 barang
-
-
-# In[ ]:
-
 
 sns.countplot(x = 'Item', data = df, order = df['Item'].value_counts().iloc[:90].index)
 plt.xticks(rotation=90)  
@@ -38,30 +20,15 @@ plt.xticks(rotation=90)
 # - menghapus data yang kosong
 # - transformasi data ke bentuk tabular 1 atau 0
 
-# In[ ]:
-
-
 data=df.drop(['Tanggal','Hotel','Qty','Satuan','Harga Jual',
               'Total Jual','Jumlah','Harga Beli','Total Harga Beli',
               'Ongkos Angkut','Profit','%'],axis=1)
 data
 
-
-# In[ ]:
-
-
 data.isna().sum()
-
-
-# In[ ]:
-
 
 data.dropna(axis=0, subset=['Item'], inplace=True)
 data
-
-
-# In[ ]:
-
 
 dataset = data.groupby(['No Do','Item']).size().reset_index(name='count')
 basket = (dataset.groupby(['No Do', 'Item'])['count']
@@ -69,10 +36,7 @@ basket = (dataset.groupby(['No Do', 'Item'])['count']
           .set_index('No Do'))
 basket
 
-
-# In[ ]:
-
-
+#transformasi data
 def encode_units(x) :
     if x <= 0:
         return 0
@@ -81,36 +45,21 @@ def encode_units(x) :
 basket_sets = basket.applymap(encode_units)
 basket_sets
 
-
-# In[ ]:
-
-
+#filter basket > 2
 basket_filter = basket_sets[(basket_sets > 0).sum(axis=1) >=2]
 basket_filter
 
-
-# # Data Mining
-
-# In[ ]:
-
-
+# Data Mining
 frequent_itemsets = apriori(basket_sets, min_support=0.1, use_colnames=True)
 frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
 frequent_itemsets
-
-
-# In[ ]:
-
 
 # Create the rules
 rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.65)
 hasil = rules[['antecedents','consequents','support','confidence']]
 hasil
 
-
-# In[ ]:
-
-
+#Pembentukan Aturan Asosiasi Final
 hasil = rules[(rules['confidence'] >=0.65) &
            (rules['lift'] >1)]
 apr_result = hasil.sort_values(by='lift', ascending=False)
